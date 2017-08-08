74fdbe5a422a4a39d9a60747d8cd189905164717
Jacob Penney <jacobpenney@Jacobs-MacBook-Pro.local>
2017-08-08 15:32:40 -0700

    formatted image submit handler to format metaData api response

--- client/src/helpers/helpers.js
+++ client/src/helpers/helpers.js
@@ -23,23 +23,48 @@ const validFileType = function (file) {
   return false;
 }
 
+/*
+* takes in the total bytes in a photo, and parses them into 
+* more meaningful units: kilobytes(KB) or megabytes(MB)
+* bonus fact: a kilobyte is 1024 bytes rather than 1000
+* due to the scalability of binary data: 2^10 = 1024.
+* Bonus bonus fact: there are 2^10 KBs in a MB!
+*/
 const returnFileSize = function(number) {
   if(number < 1024) {
     return number + 'bytes';
   } else if(number > 1024 && number < 1048576) {
+    //this block runs if the file size is at leadt 1KB, 
+    //but less than 1MB; bytes are converted to KBs. 
     return (number/1024).toFixed(1) + 'KB';
   } else if(number > 1048576) {
     return (number/1048576).toFixed(1) + 'MB';
   }
 }
 
+/*
+* below, updateImage is exported. From there is imported
+* into index.jsx, bound to its context, then passed to
+* to Update.jsx, where it is called when there is a 
+* change in the input element accepting photo uploads
+*/
 const updateImage = function() {
+  //preview and input defined in index.jsx, they are 
+  //DOM elements selected using query selectors. 
   while(this.preview.firstChild) {
+    //if the preview element has a child populating it,
+    //remove it, then check again
     this.preview.removeChild(this.preview.firstChild);
   }
+  //curFiles points to the files currently staged in
+  //the input DOM element
   let curFiles = this.input.files;
+  //we can set state from this file because it will be 
+  //bound to the App in index.jsx
   this.setState({photo: curFiles})
   if(curFiles.length === 0) {
+    //while there are no files staged,
+    //display a text element indicating the empty input
     let para = document.createElement('h2');
     para.textContent = 'No files currently selected for upload';
     this.preview.appendChild(para);
