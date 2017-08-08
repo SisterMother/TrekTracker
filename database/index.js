067be220b1fe00a4dd87d1ac48bcb7cacabb981a
Thomas Volk <tvolk131@gmail.com>
2017-08-08 11:40:22 -0700

    Has main database functions

--- database/index.js
+++ database/index.js
@@ -1,39 +1,65 @@
 var helpers = require('./helpers.js');
 
-module.exports.getUserById = () => {
+
+
+module.exports.getUserById = (id) => {
+  return helpers.getUser({id});
 };
 
-module.exports.getUserByEmail = () => {
+module.exports.getUserByEmail = (email) => {
+  return helpers.getUser({email});
 };
 
 module.exports.getAllUsers = () => {
+  return helpers.getUsers();
 };
 
 
-module.exports.getTrailById = () => {
+
+module.exports.getTrailById = (id) => {
+  return helpers.getTrail({id});
 };
 
-module.exports.getTrailByName = () => {
+module.exports.getTrailByName = (name) => {
+  return helpers.getTrail({name});
 };
 
 module.exports.getAllTrails = () => {
+  return helpers.getTrails();
 };
 
-module.exports.createTrail = () => {
+module.exports.createTrail = (name) => {
+  return helpers.createTrail(name);
 };
 
 
-module.exports.getPostsByUserId = () => {
+
+module.exports.getPostById = (id) => {
+  return helpers.getPost({id});
 };
 
-module.exports.getPostsByUserEmail = () => {
+module.exports.getPostsByUserId = (id) => {
+  return helpers.getPosts({poster_user_id: id});
 };
 
-module.exports.getPostsByTrailId = () => {
+module.exports.getPostsByUserEmail = (email) => {
+  return module.exports.getUserByEmail(email)
+  .then((user) => {
+    return helpers.getPosts({poster_user_id: user.id});
+  });
 };
 
-module.exports.getPostsByTrailName = () => {
+module.exports.getPostsByTrailId = (id) => {
+  return helpers.getPosts({trail_id: id});
 };
 
-module.exports.createPost = () => {
+module.exports.getPostsByTrailName = (name) => {
+  return module.exports.getTrailByName(name)
+  .then((trail) => {
+    return helpers.getPosts({trail_id: trail.id});
+  });
+};
+
+module.exports.createPost = (posterId, trailId, title, text, imageUrl) => {
+  return helpers.createPost(posterId, trailId, title, text, imageUrl);
 };
