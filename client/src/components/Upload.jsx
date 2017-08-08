67b5d26608a33bf06f29acf2a4e8f639630332f1
Jacob Penney <jacobpenney@Jacobs-MacBook-Pro.local>
2017-08-08 12:14:50 -0700

    upload component, upload img to DOM

---
+++ client/src/components/Upload.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Upload = (props) => (
+  <div>
+    <form onSubmit={props.submit}>
+      <h2>Upload trek pic!</h2>
+      <input className='input' onChange={() => props.update()} type="file"/>
+      <button style={{position:"relative",left:"10px",backgroundColor:"papayawhip"}}>Submit</button>
+    </form>
+    <div className="preview">
+      <p>No files currently selected for upload</p>
+    </div>
+  </div>
+);
+
+export default Upload;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import $ from 'jquery';
+import axios from 'axios';
 import reactDOM from 'react-dom';
 import Login from './components/Login.jsx';
+import Upload from './components/Upload.jsx';
+import updateImage from './helpers/helpers.js';
 import {BrowserRouter, HashRouter, Route, Switch} from 'react-router-dom';
 
 class App extends React.Component {
@@ -9,8 +12,63 @@ class App extends React.Component {
     super(props);
     this.state = {
       posts: [],
-      logged: false
+      logged: false,
+      image: null,
+      photo: null
     }
+    this.submitImage = this.submitImage.bind(this);
+    this.updateImageDisplay = updateImage.updateImage.bind(this);
+  }
+  
+  // var form = new FormData();
+  //   form.append("image", this.state.photo[0]);
+  //   //post to the database: 
+  //   $.ajax({
+  //     "async": true,
+  //     "crossDomain": true,
+  //     "url": "https://api.imgur.com/3/image",
+  //     "method": "POST",
+  //     "headers": {
+  //       "authorization": "Client-ID 3ec73e8df33fffc"
+  //     },
+  //     "processData": false,
+  //     "contentType": false,
+  //     "data": form
+  //   }).done((response) => {
+  //     //then add the link and other metadata to mysql database
+  //     var name = this.state.photo[0].name;
+  //     axios.post('/images', {image: response['data'], state: this.state, name: name})
+  //       .then((res) => {
+  //         console.log('success!: ', res);
+  //       })
+  //   });
+
+  submitImage(e) {
+    e.preventDefault();
+    var form = new FormData();
+    form.append('image', this.state.image)
+    console.log('event: ', e);
+    // axios.post('https://api.imgur.com/3/image', {
+    //   headers: {
+    //     authorization: 'Client-ID 19f99ae10e881f3'
+    //   },
+    //   crossDomain: true,
+    //   processData: false,
+    //   contentType: false,
+    //   data: form
+    // })
+    // .then((res) => {
+    //   var name = this.state.photo.name;
+    //   axios.post('/images', {image: res.data, state: this.state, name: name})
+    //     .then((res) => {
+    //       console.log('success!: ', res);
+    //   })
+    // })
+  }
+
+  componentDidMount() {
+    this.input = document.querySelector('.input');
+    this.preview = document.querySelector('.preview');
   }
 
   render() {
@@ -20,6 +78,7 @@ class App extends React.Component {
         <Switch>
           <Route path='/' component={Login}/>
         </Switch>
+        <Upload update={this.updateImageDisplay} submit={this.submitImage}/>
       </div>
     )
   }
