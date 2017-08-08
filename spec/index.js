0e654bc58a2fd72a7c8af43ccc7dd4a1bd2d3746
Thomas Volk <tvolk131@gmail.com>
2017-08-08 16:44:14 -0700

    Doesn't have dummy test

--- spec/index.js
+++ spec/index.js
@@ -2,9 +2,3 @@
 // directory and link them up to this file so they will run
 
 var expect = require('chai').expect;
-
-describe('Example', () => {
-  it('Should pass', () => {
-    expect(true).to.equal(true);
-  });
-});
