6fdbb4cf167e3a8bd04c2eca5df7adba7e5887a5
Thomas Volk <tvolk131@gmail.com>
2017-08-08 15:07:39 -0700

    Has support for mocha/chai testing

---
+++ spec/index.js
@@ -0,0 +1,10 @@
+// Place all tests in appropriately named files within the test
+// directory and link them up to this file so they will run
+
+var expect = require('chai').expect;
+
+describe('Example', () => {
+  it('Should pass', () => {
+    expect(true).to.equal(true);
+  });
+});
