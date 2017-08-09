931dc02acd8a7b270622328be0b527816126c694
Thomas Volk <tvolk131@gmail.com>
2017-08-08 17:29:26 -0700

    Server has core components and basic api router

--- server/index.js
+++ server/index.js
@@ -1,27 +1,22 @@
 var express = require('express');
 var axios = require('axios');
 var app = express();
-
 var bodyParser = require('body-parser');
-var post = require('../database');
+var db = require('../database');
+var apiRouter = require('./api-router.js');
 
+// Setup API routes
+app.use('/api', apiRouter);
 
-app.use(express.static(__dirname + '/../client/dist'));
-
-app.post('/images', (req, res) => {
-  axios.post()
+// Serve static files
+app.get('/bundle.js', (req, res) => {
+  res.sendFile('../client/dist/bundle.js');
+});
+app.get('/*', (req, res) => {
+  res.sendFile('../client/dist/');
 });
 
-// app.get('/post', function (req, res) {
-//   post.selectAll(function(err, data) {
-//     if(err) {
-//       res.sendStatus(500);
-//     } else {
-//       res.json(data);
-//     }
-//   });
-// });
-
+// Start server
 app.listen(3000, function() {
   console.log('listening on port 3000...');
 });
