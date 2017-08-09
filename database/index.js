2892afd78750ed68511a503baa8d1a37017dc3c4
Thomas Volk <tvolk131@gmail.com>
2017-08-09 14:29:39 -0700

    Has unuseful functions commented out

--- database/index.js
+++ database/index.js
@@ -3,23 +3,23 @@ var models = require('./models');
 
 
 
-module.exports.getUserById = (id) => {
-  return helpers.getUser({id});
-};
+// module.exports.getUserById = (id) => {
+//   return helpers.getUser({id});
+// };
 
 module.exports.getUserByEmail = (email) => {
   return helpers.getUser({email});
 };
 
-module.exports.getAllUsers = () => {
-  return helpers.getUsers();
-};
+// module.exports.getAllUsers = () => {
+//   return helpers.getUsers();
+// };
 
 
 
-module.exports.getTrailById = (id) => {
-  return helpers.getTrail({id});
-};
+// module.exports.getTrailById = (id) => {
+//   return helpers.getTrail({id});
+// };
 
 module.exports.getTrailByName = (name) => {
   return helpers.getTrail({name});
@@ -35,13 +35,13 @@ module.exports.createTrail = (name) => {
 
 
 
-module.exports.getPostById = (id) => {
-  return helpers.getPost({id});
-};
+// module.exports.getPostById = (id) => {
+//   return helpers.getPost({id});
+// };
 
-module.exports.getPostsByUserId = (id) => {
-  return helpers.getPosts({poster_user_id: id});
-};
+// module.exports.getPostsByUserId = (id) => {
+//   return helpers.getPosts({poster_user_id: id});
+// };
 
 module.exports.getPostsByUserEmail = (email) => {
   return module.exports.getUserByEmail(email)
@@ -57,9 +57,9 @@ module.exports.getPostsByUserEmail = (email) => {
   });
 };
 
-module.exports.getPostsByTrailId = (id) => {
-  return helpers.getPosts({trail_id: id});
-};
+// module.exports.getPostsByTrailId = (id) => {
+//   return helpers.getPosts({trail_id: id});
+// };
 
 module.exports.getPostsByTrailName = (name) => {
   return module.exports.getTrailByName(name)
