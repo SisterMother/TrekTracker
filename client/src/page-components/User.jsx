3b2930872686b5f12fe4a59284e2951115d849a1
Thomas Volk <tvolk131@gmail.com>
2017-08-10 12:19:47 -0700

    Users and trails pages show correct posts

--- client/src/page-components/User.jsx
+++ client/src/page-components/User.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import axios from 'axios';
+import Posts from '../components/Posts.jsx';
 
 class User extends React.Component {
   constructor(props) {
     super(props);
-    // Gets trail name based on the URL the user is on
+
+    // Gets user email based on the URL the user is on
     let locationSplit = window.location.href.split('/');
-    this.user = locationSplit[locationSplit.length - 1];
+    this.state = {
+      user: locationSplit[locationSplit.length - 1],
+      posts: []
+    };
+
+    axios.get('/api/posts/users/' + this.state.user)
+    .then((response) => {
+      this.setState({posts: response.data});
+    });
   }
 
   render() {
     return (
       <div>
-        {'Path: ' + this.user}
+        <Posts posts={this.state.posts} />
       </div>
     );
   }
