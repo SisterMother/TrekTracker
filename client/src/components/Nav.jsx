19753bfb1c7e328c2f0e93f4e4750fe850430ab0
Jacob Penney <jacobpenney@Jacobs-MacBook-Pro.local>
2017-08-10 14:33:55 -0700

    added nav bar component

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
 
+//&#9776; is a special unicode value which renders to the 
+//icon of the three horizonal bars representing the menu
 const Nav = (props) => (
   <div className="nav">
-    <h2>Trek Tracker!</h2>
+    <ul>
+      <div className="title">Trek Tracker</div>
+      <div className="dropdown">
+        <button className="dropbtn">&#9776;</button>
+        <div className="dropdown-content">
+          <Link to='/upload'><a>Upload</a></Link>
+          <Link to='/profile'><a>Profile</a></Link>
+        </div>
+      </div>
+    </ul>
   </div>
 );
 
