f4bb3677f5ac60354118bf921a34af87a9af7f33
Thomas Volk <tvolk131@gmail.com>
2017-08-10 11:39:20 -0700

    Client has basic page routes

---
+++ client/src/page-components/User.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import axios from 'axios';
+
+class User extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        User
+      </div>
+    );
+  }
+}
+
+export default User;
