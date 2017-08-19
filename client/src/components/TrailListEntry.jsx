583e1981d097e8686991f212d6d146dc3f5acf60
Samuel Davis <davisss64@gmail.com>
2017-08-19 14:51:17 -0700

    Changed to pass tests

--- client/src/components/TrailListEntry.jsx
+++ client/src/components/TrailListEntry.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const ListEntry = (props) => (
-<div className = 'TrailListEntry'>
-<h2>{props.currentMarker.name} </h2>
-<h3>{props.currentMarker.city}</h3>
-<h3>{props.currentMarker.state}</h3>
-</div>
+  <div className = 'TrailListEntry'>
+    <h2>{props.currentMarker.name} </h2>
+    <h3>{props.currentMarker.city}</h3>
+    <h3>{props.currentMarker.state}</h3>
+  </div>
 
 )
 
