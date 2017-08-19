587971ddd01a41d00f1a0170b47199680ce9effd
Samuel Davis <davisss64@gmail.com>
2017-08-19 08:55:15 -0700

    Working on modifying markers.

---
+++ client/src/components/TrailListEntry.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ListEntry = (props) => (
+<div className = 'TrailListEntry'>
+<h2>{props.currentMarker.name} </h2>
+<h3>{props.currentMarker.city}</h3>
+<h3>{props.currentMarker.state}</h3>
+</div>
+
+)
+
+
+
+export default ListEntry;
