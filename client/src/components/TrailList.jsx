155a79b62e30ab5a6df5175eb23a7758bf9a227f
Thomas Volk <tvolk131@gmail.com>
2017-08-20 13:53:37 -0700

    TrailList component is called TrailList

--- client/src/components/TrailList.jsx
+++ client/src/components/TrailList.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ListEntry from './TrailListEntry.jsx'
 
-const List = (props) => (
+const TrailList = (props) => (
   <div className = 'TrailList'>
     <ul> {props.markers.map((marker, index) =>
       <ListEntry key={index} trail={marker} onClick={props.onClick} />
@@ -10,4 +10,4 @@ const List = (props) => (
   </div>
 )
 
-export default List
+export default TrailList
