ee485e7addace697e6e375f9929a4e3136f59c70
Thomas Volk <tvolk131@gmail.com>
2017-08-20 13:58:06 -0700

    Navbar uses Material-UI

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,22 +1,57 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import AppBar from 'material-ui/AppBar';
 
-//note: &#9776; is a special unicode value which renders to the 
-//icon of the three horizonal bars representing the menu
-const Nav = (props) => (
-  <div className="nav">
-    <ul>
-      <div className="title">Trek Tracker</div>
-      <div className="dropdown">
-        <button className="dropbtn">&#9776;</button>
-        <div className="dropdown-content">
-          <a href='/upload'>Upload</a>
-          <a href='/profile'>Profile</a>
-          <a href='/logout'>Logout</a>
-        </div>
+class Navbar extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      open: false
+    }
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    if (this.props.setToggle) {
+      this.props.setToggle(this.handleToggle);
+    }
+    if (this.props.setClose) {
+      this.props.setClose(this.handleClose);
+    }
+  }
+
+  handleToggle () {
+    this.setState({open: !this.state.open});
+  }
+  handleClose () {
+    this.setState({open: false});
+  }
+
+  redirectTo (url) {
+    if (!this.isCurrentUrl(url)) {
+      window.location.replace(url);
+    }
+  }
+
+  isCurrentUrl (url) {
+    return (window.location.pathname === url);
+  }
+
+  render () {
+    return (
+      <div>
+        <AppBar
+          title="TrekTracker"
+          onLeftIconButtonTouchTap={this.handleToggle}
+        />
+        <Drawer docked={false} width={250} open={this.state.open} onRequestChange={(open) => this.setState({open})}>
+          <MenuItem onClick={this.redirectTo.bind(this, '/')}>Home</MenuItem>
+          <MenuItem onClick={this.redirectTo.bind(this, '/upload')}>Upload</MenuItem>
+          <MenuItem onClick={this.redirectTo.bind(this, '/profile')}>Profile</MenuItem>
+          <MenuItem onClick={this.redirectTo.bind(this, '/logout')}>Logout</MenuItem>
+        </Drawer>
       </div>
-    </ul>
-  </div>
-);
+    )
+  }
+}
 
-export default Nav;
+export default Navbar;
