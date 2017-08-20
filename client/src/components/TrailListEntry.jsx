469332624e31d70a1532227204fc6b7af37ed4e1
Thomas Volk <tvolk131@gmail.com>
2017-08-20 13:52:35 -0700

    Trail List uses Material-UI cards

--- client/src/components/TrailListEntry.jsx
+++ client/src/components/TrailListEntry.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-const ListEntry = (props) => (
-  <div className = 'TrailListEntry'  onClick = {props.onClick.bind(this, props.trail)} >
-    <h2>{props.trail.name} </h2>
-    <h3>{props.trail.city}</h3>
-    <h3>{props.trail.state}</h3>
-  </div>
-)
+import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card'
+import FlatButton from 'material-ui/FlatButton'
 
-export default ListEntry;
+class TrailListEntry extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <Card>
+        <CardHeader
+          title={this.props.trail.name}
+          subtitle={this.props.trail.city + ', ' + this.props.trail.state}
+        />
+        <CardActions>
+          <FlatButton label='View on Map' onClick={this.props.onClick.bind(this, this.props.trail)} />
+        </CardActions>
+      </Card>
+    );
+  }
+}
+
+export default TrailListEntry;
