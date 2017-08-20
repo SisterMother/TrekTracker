f3c4f9466f19b798b798a6a60407c8fe99117785
Thomas Volk <tvolk131@gmail.com>
2017-08-20 13:29:21 -0700

    TrailsList entries have unique keys to prevent React warning

--- client/src/components/TrailList.jsx
+++ client/src/components/TrailList.jsx
@@ -3,8 +3,8 @@ import ListEntry from './TrailListEntry.jsx'
 
 const List = (props) => (
   <div className = 'TrailList'>
-    <ul> {props.markers.map(marker =>
-      <ListEntry trail={marker} onClick={props.onClick} />
+    <ul> {props.markers.map((marker, index) =>
+      <ListEntry key={index} trail={marker} onClick={props.onClick} />
       )}
     </ul>
   </div>
--- client/src/components/TrailListEntry.jsx
+++ client/src/components/TrailListEntry.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 const ListEntry = (props) => (
-  <div className = 'TrailListEntry'  onClick = {props.onClick.call(this, props.trail)} >
+  <div className = 'TrailListEntry'  onClick = {props.onClick.bind(this, props.trail)} >
     <h2>{props.trail.name} </h2>
     <h3>{props.trail.city}</h3>
     <h3>{props.trail.state}</h3>
