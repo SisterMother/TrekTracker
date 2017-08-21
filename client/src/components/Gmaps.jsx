2dc56c5e45d6365d830962adf9d2507af6849baf
OneCent01 <jmpenney22@gmail.com>
2017-08-21 15:09:20 -0700

    cleaned up codebase

--- client/src/components/Gmaps.jsx
+++ client/src/components/Gmaps.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import { withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps'
 import SearchBox from 'react-google-maps/lib/places/SearchBox'
-import { handlePlacesChanged } from '../helpers/helpers.js'
 import PopupBox from './PopupBox.jsx'
 const INPUT_STYLE = {
   boxSizing: `border-box`,
