56643e7ecb457f869d2bf2fdd6a904a77515d586
interdigitize <interdigitize@users.noreply.github.com>
2017-08-23 19:35:38 -0700

    Add events to mockdb.json

--- spec/database.js
+++ spec/database.js
@@ -58,6 +58,11 @@ module.exports.run = () => {
             models.trails.create(db.trails[i])
           );
         }
+        for (let i = 0; i < db.events.length; i++) {
+          promises.push(
+            models.events.create(db.events[i])
+          );
+        }
         return Promise.all(promises);
       });
     });
@@ -202,6 +207,33 @@ module.exports.run = () => {
       });
     });
 
+    describe('createEvent()', () => {
+      it('Should exist', () => {
+        expect(dbFuncs.createEvent).to.exist;
+      });
+      it('Should be a function', () => {
+        expect(dbFuncs.createEvent).to.be.a('function');
+      });
+      it('Should create an event with valid parameters', () => {
+        let title = 'Join us for a view from the Top';
+        let desc = 'Beautiful vistas are better with friends, so come.';
+        let start = '2017, 10, 2, 17, 55';
+        let end = '2017, 10, 2, 20, 00';
+        let contact = '123@thats.me';
+        return dbFuncs.createEvent(db.users[0].email, db.trails[0].id, title, desc, start, end, contact)
+        .then((event) => {
+          expect(event).to.exist;
+          expect(event.createdAt).to.exist;
+          expect(event.updatedAt).to.exist;
+          expect(event.title).to.equal(title);
+          expect(event.desc).to.equal(desc);
+          expect(event.start).to.equal(start);
+          expect(event.end).to.equal(end);
+          expect(event.contact).to.equal(contact);
+        });
+      });
+    })
+
     describe('createPost()', () => {
       it('Should exist', () => {
         expect(dbFuncs.createPost).to.exist;
@@ -293,5 +325,6 @@ module.exports.run = () => {
         });
       });
     });
+
   });
 };
